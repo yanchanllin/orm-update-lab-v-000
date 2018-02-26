dd26d815adea5edfc290ebf65100d70a3c89cf31
YanC Lin <yonalin1@gmail.com>
2018-02-26 21:58:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,8 @@ class Student
     end
   end
 
-  def self.create
-    student = Student.new
+  def self.create(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end
