eada06ea519933a03bde0691faeb1526256e6c64
YanC Lin <yonalin1@gmail.com>
2018-02-26 22:27:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,9 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name, grade,id=nil)
-    @name = name
-    @grade = grade
-    @id = id
+    # @name = name
+    # @grade = grade
+    # @id = id
   end
   def self.create_table
     sql =  <<-SQL
